e462f0978e2d7425b12a3381b81004dce2a47375
mijkolsmith <mijkolsmith@live.com>
2022-02-17 16:50:49 +0100

    you can now succesfully copy files into a project folder, and basic autosorting works as well. Not sure if there's much bugs. Next I should limit the filetypes (It accepts folder but it crashes, for example

--- K3-TOOLS/Form1.Designer.cs
+++ K3-TOOLS/Form1.Designer.cs
@@ -33,61 +33,92 @@ namespace K3_TOOLS
 			this.testLabel = new System.Windows.Forms.Label();
 			this.label1 = new System.Windows.Forms.Label();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.baseDirectoryLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// sortButton
 			// 
-			this.sortButton.Location = new System.Drawing.Point(146, 412);
+			this.sortButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.sortButton.Location = new System.Drawing.Point(192, 603);
 			this.sortButton.Name = "sortButton";
 			this.sortButton.Size = new System.Drawing.Size(75, 23);
-			this.sortButton.TabIndex = 0;
+			this.sortButton.TabIndex = 1;
 			this.sortButton.Text = "Sort";
 			this.sortButton.UseVisualStyleBackColor = true;
 			this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
 			// 
 			// testLabel
 			// 
+			this.testLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.testLabel.AutoSize = true;
-			this.testLabel.Location = new System.Drawing.Point(154, 445);
+			this.testLabel.Location = new System.Drawing.Point(190, 629);
 			this.testLabel.Name = "testLabel";
-			this.testLabel.Size = new System.Drawing.Size(58, 17);
-			this.testLabel.TabIndex = 1;
-			this.testLabel.Text = "testText";
+			this.testLabel.Size = new System.Drawing.Size(77, 17);
+			this.testLabel.TabIndex = 2;
+			this.testLabel.Text = "Not sorting";
 			// 
 			// label1
 			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(884, 483);
+			this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+			this.label1.Location = new System.Drawing.Point(916, 647);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(134, 17);
-			this.label1.TabIndex = 2;
+			this.label1.TabIndex = 5;
 			this.label1.Text = "made by mijkolsmith";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// panel1
 			// 
 			this.panel1.AllowDrop = true;
+			this.panel1.AutoScroll = true;
 			this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
 			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
 			this.panel1.Location = new System.Drawing.Point(0, 0);
 			this.panel1.Margin = new System.Windows.Forms.Padding(0);
 			this.panel1.Name = "panel1";
-			this.panel1.Padding = new System.Windows.Forms.Padding(50, 50, 0, 0);
-			this.panel1.Size = new System.Drawing.Size(1030, 400);
-			this.panel1.TabIndex = 3;
+			this.panel1.Size = new System.Drawing.Size(1062, 540);
+			this.panel1.TabIndex = 6;
+			this.panel1.TabStop = true;
 			this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
 			this.panel1.DragOver += new System.Windows.Forms.DragEventHandler(this.panel1_DragOver);
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.button1.Location = new System.Drawing.Point(22, 603);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(148, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Set Project Directory";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// baseDirectoryLabel
+			// 
+			this.baseDirectoryLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.baseDirectoryLabel.AutoSize = true;
+			this.baseDirectoryLabel.Location = new System.Drawing.Point(435, 550);
+			this.baseDirectoryLabel.Name = "baseDirectoryLabel";
+			this.baseDirectoryLabel.Size = new System.Drawing.Size(192, 17);
+			this.baseDirectoryLabel.TabIndex = 3;
+			this.baseDirectoryLabel.Text = "Project Directory Placeholder";
 			// 
 			// Form1
 			// 
 			this.AllowDrop = true;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1030, 509);
-			this.Controls.Add(this.panel1);
+			this.ClientSize = new System.Drawing.Size(1062, 673);
+			this.Controls.Add(this.baseDirectoryLabel);
+			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.testLabel);
 			this.Controls.Add(this.sortButton);
+			this.Controls.Add(this.panel1);
 			this.Name = "Form1";
 			this.Text = "Project Sorter Tool";
 			this.ResumeLayout(false);
@@ -101,6 +132,8 @@ namespace K3_TOOLS
 		private System.Windows.Forms.Label testLabel;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label baseDirectoryLabel;
 	}
 }
 
--- K3-TOOLS/Form1.cs
+++ K3-TOOLS/Form1.cs
@@ -1,51 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace K3_TOOLS
 {
     public enum FileType
     {
         Unknown,
-        Image,
-        Model,
-        Script
+        Images,
+        Audio,
+        Video,
+        Models,
+        Prefabs,
+        Scripts,
+        Cpp,
+        Python,
+        Html,
+        Css,
+        JavaScript,
+        Php,
+        Xml
     }
 
 	public partial class Form1 : Form
 	{
-        Dictionary<string, FileType> files;
-        bool isImage;
+        string baseDirectory;
+        Dictionary<string, FileType> files = new Dictionary<string, FileType>();
+        List<Label> labels = new List<Label>();
+        int displayedAmount;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        //https://www.c-sharpcorner.com/blogs/drag-and-drop-file-on-windows-forms1
         private FileType GetFileType(string filePath)
         {
             string ext = Path.GetExtension(filePath).ToLower();
-            if ((ext == ".jpg") || (ext == ".png") || (ext == ".bmp"))
+            if ((ext == ".jpg") || (ext == ".png") || (ext == ".bmp") || (ext == ".apng") || (ext == ".avif") || (ext == ".gif") || (ext == ".jpeg") || (ext == ".jfif") || (ext == ".pjpeg") || (ext == ".pjp") || (ext == ".webp"))
             {
-                return FileType.Image;
+                return FileType.Images;
             }
+            if ((ext == ".wav") || (ext == ".mp3") || (ext == ".aac") || (ext == ".aiff") || (ext == ".alac") || (ext == ".flac") || (ext == ".m4a") || (ext == ".ogg") || (ext == ".mogg") || (ext == ".oga") || (ext == ".wma"))
+            {
+                return FileType.Audio;
+            }
+            if ((ext == ".mp4") || (ext == ".wmv") || (ext == ".mov") || (ext == ".avi") || (ext == ".avchd") || (ext == ".flv") || (ext == ".f4v") || (ext == ".swf") || (ext == ".mkv") || (ext == ".webm") || (ext == ".html5") || (ext == ".ts") || (ext == ".ts") || (ext == ".amv") || (ext == ".m4v") || (ext == ".m4p") || (ext == ".mpg") || (ext == ".mpeg") || (ext == ".m2v") || (ext == ".mpv") || (ext == ".m4v") || (ext == ".3gp") || (ext == ".3g2"))
+            {
+                return FileType.Video;
+            }
+            if ((ext == ".gtlf") || (ext == ".glb") || (ext == ".fbx") || (ext == ".obj") || (ext == ".usdz") || (ext == ".usd") || (ext == ".stl") || (ext == ".max") || (ext == ".x3d") || (ext == ".vrml") || (ext == ".3mf") || (ext == ".dae"))
+            {
+                return FileType.Models;
+            }
+            if (ext == ".cs")
+            {
+                return FileType.Scripts;
+            }
+            if ((ext == ".html") || (ext == ".htm") || (ext == ".xhtml") || (ext == ".jhtml"))
+            {
+                return FileType.Html;
+            }
+            if (ext == ".css")
+            {
+                return FileType.Css;
+            }
+            if (ext == ".js")
+            {
+                return FileType.JavaScript;
+            }
+            if ((ext == ".php") || (ext == ".php4") || (ext == ".php3") || (ext == ".phtml"))
+            {
+                return FileType.Php;
+            }
+            if ((ext == ".xml") || (ext == ".rss") || (ext == ".svg"))
+            {
+                return FileType.Xml;
+            }
+            if ((ext == ".xml") || (ext == ".rss") || (ext == ".svg"))
+            {
+                return FileType.Xml;
+            }
+            if ((ext == ".cc") || (ext == ".cpp") || (ext == ".h"))
+			{
+                return FileType.Cpp;
+			}
+            if ((ext == ".py") || (ext == ".pyc") || (ext == ".pyo") || (ext == ".pyd"))
+			{
+                return FileType.Python;
+			}
+
             else return FileType.Unknown;
         }
-
+        
+        //https://www.c-sharpcorner.com/blogs/drag-and-drop-file-on-windows-forms1
         private void panel1_DragDrop(object sender, DragEventArgs e)
         {
-            foreach (string file in e.Data.GetData("FileDrop") as string[])
+            foreach (string file in e.Data.GetData(DataFormats.FileDrop) as string[])
             {
-                files.Add(file, GetFileType(file));
-                Label fileLabel = new Label();
-                fileLabel.Text = file;
+                if (!files.ContainsKey(file))
+                {
+                    files.Add(file, GetFileType(file));
+                    Label fileLabel = new Label();
+                    fileLabel.Text = file;
+                    fileLabel.Location = new Point(0, 0 + 12 * displayedAmount);
+                    fileLabel.AutoSize = true;
+                    panel1.Controls.Add(fileLabel);
+
+                    labels.Add(fileLabel);
+                    displayedAmount++;
+                }
             }
         }
 
@@ -57,7 +126,51 @@ namespace K3_TOOLS
 
         private void sortButton_Click(object sender, EventArgs e)
         {
-            testLabel.Text = "sorting...";
+            if (baseDirectory == null)
+            {
+                testLabel.Text = "Project directory not set";
+                return;
+            }
+            testLabel.Text = "Sorting...";
+
+            SortFiles();
+
+            labels.RemoveAll(x => x.GetType() == typeof(Label));
+            labels.Clear();
+            displayedAmount = 0;
         }
-    }
+
+        //A file sorter. This function creates new folders for every file in files based on their file type and sorts them there.
+        private void SortFiles()
+		{
+            foreach (var fileType in files.Values.ToList())
+            {
+                var destinationPath = Path.Combine(baseDirectory, fileType.ToString());
+                if (!Directory.Exists(destinationPath))
+                {
+                    Directory.CreateDirectory(Path.Combine(destinationPath)); 
+                }
+                foreach (string file in files.Keys.Where(x => files[x] == fileType).ToList())
+                {
+                    File.Copy(file, Path.Combine(destinationPath, Path.GetFileName(file)), true);
+                }
+            }
+		}
+
+
+        //The OpenFolderDialog interface is awful, so I'm using a CommonOpenFileDialog. This is integrated in Microsoft.WindowsAPICodePack.Dialogs. I installed this using https://www.nuget.org/packages/Microsoft-WindowsAPICodePack-Core/ . Open View > Package Manager Console and paste this line "Install-Package Microsoft-WindowsAPICodePack-Core -Version 1.1.4". Works in Visual Studio 2019 16.11.10.
+        private void button1_Click(object sender, EventArgs e)
+		{
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "C:\\";
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+            {
+                baseDirectory = dialog.FileName;
+                baseDirectoryLabel.Text = baseDirectory;
+            }
+        }
+	}
 }
