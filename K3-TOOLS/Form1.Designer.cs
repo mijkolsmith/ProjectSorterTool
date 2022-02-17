552dc944283ce95a063ffe5cdefe5a77af3c7466
mijkolsmith <mijkolsmith@live.com>
2022-02-17 13:27:19 +0100

    Basic drag & drop functionality

--- K3-TOOLS/Form1.Designer.cs
+++ K3-TOOLS/Form1.Designer.cs
@@ -29,13 +29,78 @@ namespace K3_TOOLS
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.sortButton = new System.Windows.Forms.Button();
+			this.testLabel = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.SuspendLayout();
+			// 
+			// sortButton
+			// 
+			this.sortButton.Location = new System.Drawing.Point(146, 412);
+			this.sortButton.Name = "sortButton";
+			this.sortButton.Size = new System.Drawing.Size(75, 23);
+			this.sortButton.TabIndex = 0;
+			this.sortButton.Text = "Sort";
+			this.sortButton.UseVisualStyleBackColor = true;
+			this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+			// 
+			// testLabel
+			// 
+			this.testLabel.AutoSize = true;
+			this.testLabel.Location = new System.Drawing.Point(154, 445);
+			this.testLabel.Name = "testLabel";
+			this.testLabel.Size = new System.Drawing.Size(58, 17);
+			this.testLabel.TabIndex = 1;
+			this.testLabel.Text = "testText";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(884, 483);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(134, 17);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "made by mijkolsmith";
+			// 
+			// panel1
+			// 
+			this.panel1.AllowDrop = true;
+			this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(0);
+			this.panel1.Name = "panel1";
+			this.panel1.Padding = new System.Windows.Forms.Padding(50, 50, 0, 0);
+			this.panel1.Size = new System.Drawing.Size(1030, 400);
+			this.panel1.TabIndex = 3;
+			this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+			this.panel1.DragOver += new System.Windows.Forms.DragEventHandler(this.panel1_DragOver);
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(1030, 509);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.testLabel);
+			this.Controls.Add(this.sortButton);
+			this.Name = "Form1";
+			this.Text = "Project Sorter Tool";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button sortButton;
+		private System.Windows.Forms.Label testLabel;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel1;
 	}
 }
 
--- K3-TOOLS/Form1.cs
+++ K3-TOOLS/Form1.cs
@@ -5,16 +5,59 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace K3_TOOLS
 {
+    public enum FileType
+    {
+        Unknown,
+        Image,
+        Model,
+        Script
+    }
+
 	public partial class Form1 : Form
 	{
-		public Form1()
+        Dictionary<string, FileType> files;
+        bool isImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //https://www.c-sharpcorner.com/blogs/drag-and-drop-file-on-windows-forms1
+        private FileType GetFileType(string filePath)
+        {
+            string ext = Path.GetExtension(filePath).ToLower();
+            if ((ext == ".jpg") || (ext == ".png") || (ext == ".bmp"))
+            {
+                return FileType.Image;
+            }
+            else return FileType.Unknown;
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string file in e.Data.GetData("FileDrop") as string[])
+            {
+                files.Add(file, GetFileType(file));
+                Label fileLabel = new Label();
+                fileLabel.Text = file;
+            }
+        }
+
+		private void panel1_DragOver(object sender, DragEventArgs e)
 		{
-			InitializeComponent();
-		}
-	}
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Link;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            testLabel.Text = "sorting...";
+        }
+    }
 }
